ac80088795e653419eacae887a85e9c213856b3b
ruslankagarmanov38-ui <ruslankagarmanov38@gmail.com>
2025-11-19 01:25:04 +0300

    server.js
    Signed-off-by: ruslankagarmanov38-ui <ruslankagarmanov38@gmail.com>

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http, { cors:{origin:"*"} });
+const http = require('http').createServer(app);
+const cors = require('cors');
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(cors());
 
 let queue = [];
 let partners = {};
@@ -11,73 +15,66 @@ let online = 0;
 
 io.on("connection", socket => {
     online++;
-    io.emit("online_count", online);
+    io.emit("online", online);
 
     socket.on("find", data => {
-        socket.data = data;
-        queue.push(socket);
-        match();
-    });
+        socket.my = data;
 
-    socket.on("stop", () => {
-        queue = queue.filter(s=>s!==socket);
-    });
+        let partner = queue.find(u =>
+            u.my.gender !== undefined &&
+            u.my.search !== undefined &&
+            compatible(u.my, data)
+        );
 
-    socket.on("msg", text=>{
-        let p = partners[socket.id];
-        if(!p) return;
-        p.emit("msg", text);
-    });
+        if (partner) {
+            queue = queue.filter(u => u !== partner);
+            partners[socket.id] = partner;
+            partners[partner.id] = socket;
 
-    socket.on("disconnect", ()=>{
-        online--;
-        io.emit("online_count", online);
-
-        queue = queue.filter(s=>s!==socket);
-
-        let p = partners[socket.id];
-        if(p){
-            p.emit("partner_left");
-            delete partners[p.id];
-            delete partners[socket.id];
+            socket.emit("found");
+            partner.emit("found");
+        } else {
+            queue.push(socket);
         }
     });
 
+    socket.on("typing", () => {
+        let p = partners[socket.id];
+        if (p) p.emit("typing");
+    });
+
+    socket.on("msg", txt => {
+        let p = partners[socket.id];
+        if (p) p.emit("msg", txt);
+    });
+
+    socket.on("stop", () => endChat(socket));
+
+    socket.on("disconnect", () => {
+        endChat(socket);
+        online--;
+        io.emit("online", online);
+    });
 });
 
-function match(){
-    if(queue.length < 2) return;
-
-    for(let i=0;i<queue.length;i++){
-        for(let j=i+1;j<queue.length;j++){
-            let a = queue[i];
-            let b = queue[j];
-
-            if (ok(a,b)) {
-                queue.splice(j,1);
-                queue.splice(i,1);
-
-                partners[a.id] = b;
-                partners[b.id] = a;
-
-                a.emit("found");
-                b.emit("found");
-                return;
-            }
-        }
-    }
-}
-
-function ok(a,b){
-    if(a.data.looking==="Ищу любой" && b.data.looking==="Ищу любой") return true;
-
-    if(a.data.looking==="Ищу мужчину" && b.data.gender==="Мужчина") return true;
-    if(a.data.looking==="Ищу женщину" && b.data.gender==="Женщина") return true;
-
-    if(b.data.looking==="Ищу мужчину" && a.data.gender==="Мужчина") return true;
-    if(b.data.looking==="Ищу женщину" && a.data.gender==="Женщина") return true;
-
+function compatible(a, b) {
+    if (b.search === "Ищу любой") return true;
+    if (b.search === "Ищу мужчину" && a.gender === "Мужчина") return true;
+    if (b.search === "Ищу женщину" && a.gender === "Женщина") return true;
     return false;
 }
 
-http.listen(10000, ()=>console.log("SERVER OK"));
+function endChat(s) {
+    if (queue.includes(s))
+        queue = queue.filter(u => u !== s);
+
+    let p = partners[s.id];
+    if (!p) return;
+
+    p.emit("end");
+
+    delete partners[p.id];
+    delete partners[s.id];
+}
+
+http.listen(8080, () => console.log("SERVER OK"));
